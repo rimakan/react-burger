95925ccebf086debd77ee70430ad8214dcda69c0
Kirill Fedorenko <fedorenko.kiril@gmail.com>
2023-09-24 10:39:03 +0300

    Burger-1 Implement the header

---
+++ src/components/Common/AppHeaderItem/AppHeaderItem.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import s from "./AppHeaderItem.module.scss";
+
+interface AppHeaderItemProps {
+  title: string;
+  icon: React.ReactNode;
+  isActive?: boolean;
+}
+
+const AppHeaderItem: React.FC<AppHeaderItemProps> = ({
+  title,
+  icon,
+  isActive = false,
+}) => {
+  const inactiveClassName = !isActive ? "text_color_inactive" : "";
+  const className = `text text_type_main-default ${inactiveClassName}`;
+
+  return (
+    <div className={`pl-5 pr-5 pt-4 pb-4 ${s.appHeaderItem}`}>
+      <i className={className}>{icon}</i>
+      <span className={className}>{title}</span>
+    </div>
+  );
+};
+
+export default AppHeaderItem;
---
+++ src/models/product.ts
@@ -0,0 +1,20 @@
+export enum ProductType {
+  Main = 'main',
+  Bun = 'bun',
+  Sauce = 'sauce',
+}
+
+export interface Product {
+  _id: string;
+  name: string;
+  type: string;
+  proteins: number;
+  fat: number;
+  carbohydrates: number;
+  calories: number;
+  price: number;
+  image: string;
+  image_mobile: string;
+  image_large: string;
+  __v: number;
+}
