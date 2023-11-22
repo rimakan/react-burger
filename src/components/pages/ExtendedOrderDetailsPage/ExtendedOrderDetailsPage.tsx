ac70976ec6a8ecd9460e00a5f8b3ea85287aaf32
Kirill Fedorenko <fedorenko.kiril@gmail.com>
2023-11-22 23:13:25 +0300

    RB-6 Fix long loading

--- src/components/pages/ExtendedOrderDetailsPage/ExtendedOrderDetailsPage.tsx
+++ src/components/pages/ExtendedOrderDetailsPage/ExtendedOrderDetailsPage.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import ExtendedOrderDetails from '../../Common/order/ExtendedOrderDetails/ExtendedOrderDetails';
 import { useParams } from 'react-router-dom';
 import { useSelector } from '../../../hooks';
+import { useRequestOrderDetailsPage } from './useRequestOrderDetailsPage';
 
 const ExtendedOrderDetailsPage: React.FC = () => {
+  useRequestOrderDetailsPage();
+
   const { orderId: orderIdFromUrl } = useParams();
   const orderId = Number(orderIdFromUrl);
   const order = useSelector(
@@ -11,6 +14,7 @@ const ExtendedOrderDetailsPage: React.FC = () => {
       s.user.relatedData.personalOrders?.find(({ number }) => number === orderId) ||
       s.reactBurger.orderFeed.publicOrders?.find(({ number }) => number === orderId),
   );
+
   return (
     <>
       <ExtendedOrderDetails order={order} />
